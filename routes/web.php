ab7a94e4323d95b1c88dcdf311dd68bdc1824872
Danilo Oliveira <DDanilo000@gmail.com>
2020-10-08 05:31:27 +0100

    route add

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::post('/register', function () {
+    return '';
+});
+
+Route::get('/empresa', function () {
+    return view('Sobre a empresa');
+});
+
+Route::get('/contato', function () {
+    return view('site.contact');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
